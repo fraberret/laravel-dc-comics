eeb69aaa5c9d8856852a320ec90562c1091a36c1
fraberret <f.berretta02@gmail.com>
2024-05-15 17:46:31 +0200

    add markup on show view

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="{{ $comic->thumb }}" alt="">
+            </div>
+            <div class="col">
+                <h2>{{ $comic->title }}</h2>
+                <p>{{ $comic->description }}</p>
+            </div>
+        </div>
+    </div>
 @endsection
