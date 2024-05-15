4b56acd50603fba035e550e22f818f85c5686365
fraberret <f.berretta02@gmail.com>
2024-05-15 17:28:10 +0200

    create comics index view

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -12,7 +12,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        dd(Comic::all());
+        return view('comics.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd(Comic::all());
+
     return view('welcome');
 });
 
