0f83471f80ef1e7c79ca01a71c168c908f175f30
fraberret <f.berretta02@gmail.com>
2024-05-15 17:24:50 +0200

    add comics route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComicController;
+use App\Models\Comic;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(Comic::all());
     return view('welcome');
 });
+
+Route::resource('/comics', ComicController::class);
